e11624d5503abbc00780df9211e7f8335a6a6722
LolHens <pierrekisters@gmail.com>
2018-08-30 16:56:03 +0200

    cleanup

--- server/src/main/scala/de/lolhens/MainServer.scala
+++ server/src/main/scala/de/lolhens/MainServer.scala
@@ -1,24 +1,18 @@
 package de.lolhens
 
-import java.nio.file.{Files, Paths}
-
 import de.lolhens.webserver.{PublicAssetProvider, ScalaJsScripts, WebServer}
 import monix.execution.Scheduler
+import org.http4s.HttpService
 import org.http4s.dsl.task._
-import org.http4s.{HttpService, MediaType}
 
-import scala.collection.JavaConverters._
 import scala.collection.immutable.ListMap
-import scala.util.Try
 
 object MainServer extends WebServer with PublicAssetProvider {
+  val scalaJsScripts = ScalaJsScripts("client", ScalaJsScripts.resourceExistsInClasspath("public"))
+
   lazy val jsService: Service = HttpService {
     case get@GET -> path =>
-      val jsPage = ScalaJsScripts.page(
-        "client",
-        name => s"/public/$name",
-        ScalaJsScripts.resourceExistsInClasspath("public")
-      )
+      val jsPage = scalaJsScripts.page(name => s"/public/$name")
 
       Ok(jsPage)
   }
--- server/src/main/scala/de/lolhens/webserver/ScalaJsScripts.scala
+++ server/src/main/scala/de/lolhens/webserver/ScalaJsScripts.scala
@@ -6,6 +6,49 @@ import scalatags.Text.all._
 /**
   * Created by u016595 on 14.12.2016.
   */
+case class ScalaJsScripts(projectName: String,
+                          exists: String => Boolean = ScalaJsScripts.defaultResourceExists) {
+  private val name = projectName.toLowerCase
+
+  def page(route: String => String,
+           headers: Modifier = ""): TypedTag[String] =
+    html(head(headers, tags(route)))
+
+  def tags(route: String => String): Seq[Tag] =
+    assets(route).map(ScalaJsScripts.jsScriptTag)
+
+  def assets(route: String => String): Seq[String] =
+    scripts.map(name => route(name))
+
+  def scripts: Seq[String] =
+    selectJsDeps.toSeq ++
+      selectLibrary.toSeq ++
+      selectLoader.toSeq ++
+      selectScript.toSeq
+
+  private def selectLibrary: Option[String] = Seq(
+    s"$name-opt-library.js",
+    s"$name-fastopt-library.js"
+  ).find(exists)
+
+  private def selectJsDeps: Option[String] = Seq(
+    s"$name-jsdeps.min.js",
+    s"$name-jsdeps.js"
+  ).find(exists)
+
+  private def selectLoader: Option[String] = Seq(
+    s"$name-opt-loader.js",
+    s"$name-fastopt-loader.js"
+  ).find(exists)
+
+  private def selectScript: Option[String] = Seq(
+    s"$name-opt.js",
+    s"$name-fastopt.js",
+    s"$name-opt-bundle.js",
+    s"$name-fastopt-bundle.js"
+  ).find(exists)
+}
+
 object ScalaJsScripts {
   val defaultResourceExists: String => Boolean = resourceExistsInClasspath("")
 
@@ -17,79 +60,6 @@ object ScalaJsScripts {
     Option(getClass.getResource("/" + cleanResourcePath + resource)).isDefined
   }
 
-  def page(projectName: String,
-           route: String => String,
-           exists: String => Boolean = defaultResourceExists,
-           headers: Modifier = ""): TypedTag[String] = html(
-    head(
-      headers,
-      tags(projectName, route, exists)
-    )
-  )
-
   private def jsScriptTag(source: String): Tag = script(src := source, `type` := "text/javascript")
 
-  def tags(projectName: String,
-           route: String => String,
-           exists: String => Boolean = defaultResourceExists): Seq[Tag] =
-    assets(projectName, route, exists)
-      .map(jsScriptTag)
-
-  def assets(projectName: String,
-             route: String => String,
-             exists: String => Boolean = defaultResourceExists): Seq[String] =
-    scripts(
-      projectName,
-      exists
-    )
-      .map(name => route(name))
-
-  def scripts(projectName: String,
-              exists: String => Boolean = defaultResourceExists): Seq[String] =
-    selectJsDeps(projectName, exists).toSeq ++
-      selectLibrary(projectName, exists).toSeq ++
-      selectLoader(projectName, exists).toSeq ++
-      selectScript(projectName, exists).toSeq
-
-  private def selectLibrary(projectName: String,
-                            exists: String => Boolean): Option[String] = {
-    val name = projectName.toLowerCase
-    Seq(
-      s"$name-opt-library.js",
-      s"$name-fastopt-library.js"
-    )
-      .find(exists)
-  }
-
-  private def selectJsDeps(projectName: String,
-                           exists: String => Boolean): Option[String] = {
-    val name = projectName.toLowerCase
-    Seq(
-      s"$name-jsdeps.min.js",
-      s"$name-jsdeps.js"
-    )
-      .find(exists)
-  }
-
-  private def selectLoader(projectName: String,
-                           exists: String => Boolean): Option[String] = {
-    val name = projectName.toLowerCase
-    Seq(
-      s"$name-opt-loader.js",
-      s"$name-fastopt-loader.js"
-    )
-      .find(exists)
-  }
-
-  private def selectScript(projectName: String,
-                           exists: String => Boolean): Option[String] = {
-    val name = projectName.toLowerCase
-    Seq(
-      s"$name-opt.js",
-      s"$name-fastopt.js",
-      s"$name-opt-bundle.js",
-      s"$name-fastopt-bundle.js"
-    )
-      .find(exists)
-  }
 }
